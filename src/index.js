8e3d4679414e73008e3961f9225d0479d65785a9
tarahachler <92160920+tarahachler@users.noreply.github.com>
2022-03-23 11:05:22 +0100

    csv-loader

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import * as d3 from 'd3'
 
 //Pour importer les données
 
-import file from '../data/donne_salaireBrut_2012.csv'
-import file from '../data/donne_salaireBrut_2014.csv'
-import file from '../data/donne_salaireBrut_2016.csv'
+import file1 from '../data/donnee_salaireBrut_2012.csv'
+//import file2 from '../data/donne_salaireBrut_2014.csv'
+//import file3 from '../data/donne_salaireBrut_2016.csv'
+
+ console.log(file1);
+console.log('ok');
