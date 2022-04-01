64d86041b59327a67f4e24b534a4155fa3cb42ed
Lara Hörler <lara.horler@heig-vd.ch>
2022-04-01 11:13:20 +0200

    MàJ

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import * as d3 from 'd3'
 //Pour importer les données
 
 import file1 from '../data/donnee_salaireBrut_2012.csv'
-//import file2 from '../data/donne_salaireBrut_2014.csv'
-//import file3 from '../data/donne_salaireBrut_2016.csv'
+import file2 from '../data/donnee_salaireBrut_2014.csv'
+import file3 from '../data/donnee_salaireBrut_2016.csv'
 
  console.log(file1);
-console.log('ok');
